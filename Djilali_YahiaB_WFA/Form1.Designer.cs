913bbf821b5d630a5ef62c42bb7b598bf1b6eefa
djilali yahia <DYAHIABERROUIGUET>
2023-09-21 11:44:45 +0200

    BackgroundSuperpositionImgetContrôleduJoueur

--- Djilali_YahiaB_WFA/Form1.Designer.cs
+++ Djilali_YahiaB_WFA/Form1.Designer.cs
@@ -92,7 +92,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::Djilali_YahiaB_WFA.Properties.Resources.backgroundDebut;
+            this.BackgroundImage = global::Djilali_YahiaB_WFA.Properties.Resources.backgroundDebutBT;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1477, 688);
             this.Controls.Add(this.goku);
